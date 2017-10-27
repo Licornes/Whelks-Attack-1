dcf205700354e5b37b69841357a5025859ee9688
F0xenn <F0xenn@users.noreply.github.com>
2017-10-27 13:47:49 +0200

    Whelks-Attack-1
    Premiere version de whelks-attack

---
+++ WhelksAttack/Assets/Jeu/Scripts/PersoController2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersoController2 : MonoBehaviour
+{
+
+    public Animator anim;
+    Rigidbody2D rigBody;
+    public float jumpForce = 200f;
+    public LayerMask theGround;
+    public Transform groundCheck;
+    bool onTheGround = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        onTheGround = Physics2D.Linecast(transform.position, groundCheck.position, theGround);
+        anim.SetBool("InSaut2", onTheGround);
+        if (onTheGround && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rigBody.AddForce(new Vector2(0, jumpForce));
+
+        }
+
+    }
+
+}
